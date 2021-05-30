7bc2cca019773852c89aa6dcd74277d78a35344d
Mahi Jain <Mahi Jain>
2021-05-31 00:40:24 +0530

    footer+button+other

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".nav-items");
 const nav = document.querySelector("nav");
-// hamburger.addEventListener('click', () => {
-//     navItems.classList.toggle("open");
-//     nav.style.padding = "0";
-//     nav.style.padding = "0";
-// });
+let isOpen = false;
 
-let isOpen = true;
 hamburger.addEventListener("click", () => {
-  if (isOpen) {
-    navItems.style.clipPath = "circle(800px at 50% 80%)";
-    // navItems.style.position ="fixed"
-    // nav.style.padding="0";
-    isOpen = false;
-  }else{
-    navItems.style.clipPath = "circle(10px at 50% -10%)";
-    // nav.style.padding="15px";
-    isOpen =true;
-  }
+  navItems.classList.toggle("open");
+
+//   nav.style.padding = "0";
+//   nav.style.padding = "0";
 });
+
+// hamburger.addEventListener("click", () => {
+//   if (isOpen) {
+//     navItems.style.clipPath = "circle(800px at 50% 80%)";
+//     // navItems.style.position ="fixed"
+//     // nav.style.padding="0";
+//     isOpen = false;
+//   }else{
+//     navItems.style.clipPath = "circle(10px at 50% -10%)";
+//     // nav.style.padding="15px";
+//     isOpen =true;
+//   }
+// });
